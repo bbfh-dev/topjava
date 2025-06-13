0210615a92df95ffe5c970b405b251a0b6ac155a
BubbleFish <daforsastudia@gmail.com>
2025-06-13 15:28:28 +0300

    feat: Implement MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,48 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+
+@Controller
 public class MealRestController {
-    private MealService service;
 
+    private final MealService service;
+
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public Meal create(Meal meal) {
+        return service.create(meal, SecurityUtil.authUserId());
+    }
+
+    public void delete(int id) {
+        service.delete(id, SecurityUtil.authUserId());
+    }
+
+    public Meal get(int id) {
+        return service.get(id, SecurityUtil.authUserId());
+    }
+
+    public List<MealTo> getAll() {
+        return MealsUtil.getTos(service.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay());
+    }
+
+    public List<MealTo> getFiltered(LocalTime startTime, LocalTime endTime) {
+        return MealsUtil.getFilteredTos(service.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay(), startTime, endTime);
+    }
+
+    public void update(Meal meal) {
+        assureIdConsistent(meal, meal.getId());
+        service.update(meal, SecurityUtil.authUserId());
+    }
 }

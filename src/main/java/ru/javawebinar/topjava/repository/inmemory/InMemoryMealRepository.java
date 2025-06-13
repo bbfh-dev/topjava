c19d9dbe29169b84fde6df0350ca13b051229c99
BubbleFish <daforsastudia@gmail.com>
2025-06-13 14:39:00 +0300

    feat: Make Meals be owned by specific users

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.repository.inmemory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
 import java.util.Map;
@@ -10,15 +11,21 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> mealsMap = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> usersToMealsMap = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> this.save(meal, SecurityUtil.authUserId()));
+    }
+
+    private Map<Integer, Meal> getOrInitMealsMap(int userId) {
+        return usersToMealsMap.computeIfAbsent(userId, id -> new ConcurrentHashMap<>());
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
+        Map<Integer, Meal> mealsMap = this.getOrInitMealsMap(userId);
+
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
             mealsMap.put(meal.getId(), meal);
@@ -29,17 +36,20 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public boolean delete(int id) {
+    public boolean delete(int id, int userId) {
+        Map<Integer, Meal> mealsMap = this.getOrInitMealsMap(userId);
         return mealsMap.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
+    public Meal get(int id, int userId) {
+        Map<Integer, Meal> mealsMap = this.getOrInitMealsMap(userId);
         return mealsMap.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
+    public Collection<Meal> getAll(int userId) {
+        Map<Integer, Meal> mealsMap = this.getOrInitMealsMap(userId);
         return mealsMap.values();
     }
 }

8d4fa54b6eda83e79429b01d46222f214abeb2bc
BubbleFish <daforsastudia@gmail.com>
2025-06-14 10:44:17 +0300

    feat: Add logging to MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
@@ -15,6 +17,8 @@ import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 @Controller
 public class MealRestController {
 
+    private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
+
     private final MealService service;
 
     public MealRestController(MealService service) {
@@ -22,26 +26,32 @@ public class MealRestController {
     }
 
     public Meal create(Meal meal) {
+        log.info("Create meal: {}", meal);
         return service.create(meal, SecurityUtil.authUserId());
     }
 
     public void delete(int id) {
+        log.info("Delete meal: {}", id);
         service.delete(id, SecurityUtil.authUserId());
     }
 
     public Meal get(int id) {
+        log.info("Get meal: {}", id);
         return service.get(id, SecurityUtil.authUserId());
     }
 
     public List<MealTo> getAll() {
+        log.info("Get all meals");
         return MealsUtil.getTos(service.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay());
     }
 
     public List<MealTo> getFiltered(LocalDateTime startTime, LocalDateTime endTime) {
+        log.info("Get filtered meals");
         return MealsUtil.getTos(service.getFiltered(SecurityUtil.authUserId(), startTime, endTime), SecurityUtil.authUserCaloriesPerDay());
     }
 
     public void update(Meal meal) {
+        log.info("Update meal: {}", meal);
         assureIdConsistent(meal, meal.getId());
         service.update(meal, SecurityUtil.authUserId());
     }

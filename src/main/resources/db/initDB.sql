392d6f560c32aa1872d2a35155ea794d9374e991
BubbleFish <daforsastudia@gmail.com>
2025-06-20 20:35:47 +0300

    feat: Create and populate SQL "meals" table

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_role;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -21,5 +22,16 @@ CREATE TABLE user_role
     user_id INTEGER NOT NULL,
     role    VARCHAR NOT NULL,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY      DEFAULT nextval('global_seq'),
+    user_id     INTEGER                                NOT NULL,
+    datetime    TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    description TEXT                     DEFAULT ''    NOT NULL,
+    calories    INTEGER                  DEFAULT 0     NOT NULL,
+    CONSTRAINT meals_unique_datetime_idx UNIQUE (user_id, datetime),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM users;
+DELETE
+FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +14,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, '2020-01-30 10:00:00+03', 'Завтрак', 500),
+       (100000, '2020-01-30 13:00:00+03', 'Обед', 1000),
+       (100000, '2020-01-30 20:00:00+03', 'Ужин', 500),
+       (100000, '2020-01-31 00:00:00+03', 'Еда на граничное значение', 100),
+       (100000, '2020-01-31 10:00:00+03', 'Завтрак', 1000),
+       (100000, '2020-01-31 13:00:00+03', 'Обед', 500),
+       (100000, '2020-01-31 20:00:00+03', 'Ужин', 410),
+       (100001, '2015-06-01 14:00:00+03', 'Админ ланч', 510),
+       (100001, '2015-06-01 21:00:00+03', 'Админ ужин', 1500);

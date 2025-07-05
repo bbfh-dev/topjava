f6cc02b9b5ee86583d6f0a8c8a5b12dd373e8936
BubbleFish <daforsastudia@gmail.com>
2025-07-05 15:37:16 +0300

    feat: Implement DataJpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,25 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Meal m WHERE m.id=?1 AND m.user.id=?2")
+    int delete(int id, int userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=?1 ORDER BY m.dateTime DESC")
+    List<Meal> getAll(int userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime >= ?1 AND m.dateTime < ?2 " +
+            "ORDER BY m.dateTime DESC")
+    List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, @Param("userId") int userId);
 }

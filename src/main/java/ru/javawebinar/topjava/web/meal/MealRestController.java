e20f950b9385022f2926f55ab665ccac843ba59e
BubbleFish <daforsastudia@gmail.com>
2025-07-29 11:40:04 +0300

    feat: Add @Override where applicable in MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -20,16 +20,19 @@ public class MealRestController extends AbstractMealController {
 
     static final String REST_URL = "/rest/profile/meals";
 
+    @Override
     @GetMapping
     public List<MealTo> getAll() {
         return super.getAll();
     }
 
+    @Override
     @GetMapping("/{id}")
     public Meal get(@PathVariable int id) {
         return super.get(id);
     }
 
+    @Override
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
@@ -47,6 +50,7 @@ public class MealRestController extends AbstractMealController {
         return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
+    @Override
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody Meal meal, @PathVariable int id) {
